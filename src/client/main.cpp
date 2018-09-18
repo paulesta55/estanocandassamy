d732fc7fd90e3aed1bdb5370177b8dd97dc5b2e0
paulesta55 <paul.estano@ensea.fr>
2018-09-18 16:47:08 +0200

    add some comments

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -18,9 +18,14 @@ int main(int argc,char* argv[])
 {
     Exemple exemple;
     exemple.setX(53);
+    // check the number of arguments
     if(argc != 1) {
+        // check the 1st argument
         if(strcmp(argv[1],"hello")== 0) {
             cout << "bonjour les potes" << endl;
+        }
+        else {
+            cout << "Je ne comprends pas!" << endl;
         }
     }
 
