c16dedbe88a092e0c04f80bfeb34bf3891750275
paulesta55 <paul.estano@ensea.fr>
2018-09-26 11:50:56 +0200

    new "resources" package

---
+++ cmake-build-debug/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paul/estanocandassamy/extern/jsoncpp-1.8.0/jsoncpp.cpp" "/home/paul/estanocandassamy/cmake-build-debug/CMakeFiles/client.dir/extern/jsoncpp-1.8.0/jsoncpp.cpp.o"
+  "/home/paul/estanocandassamy/src/client/main.cpp" "/home/paul/estanocandassamy/cmake-build-debug/CMakeFiles/client.dir/src/client/main.cpp.o"
+  "/home/paul/estanocandassamy/src/shared/state/ResManager.cpp" "/home/paul/estanocandassamy/cmake-build-debug/CMakeFiles/client.dir/src/shared/state/ResManager.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/client"
+  "../src/server"
+  "../src/shared"
+  "../extern/jsoncpp-1.8.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/src/client/main.cpp.o"
+  "CMakeFiles/client.dir/src/shared/state/ResManager.cpp.o"
+  "CMakeFiles/client.dir/extern/jsoncpp-1.8.0/jsoncpp.cpp.o"
+  "../bin/client.pdb"
+  "../bin/client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
