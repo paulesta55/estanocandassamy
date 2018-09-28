669e0849446fa98dc7b9620d1eb81f0a41665f00
paulesta55 <paul.estano@ensea.fr>
2018-09-28 10:37:59 +0200

    update state.png

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -19,12 +19,11 @@ int main(int argc,char* argv[])
 {
 
 
-//    ResManager *resManager = new ResManager("res/timemap.json");
-//    PixelsLoader *pixelsLoader = new PixelsLoader("res/poketile.json");
-//    if(!resManager->init()) {
-//        cout << "cannot parse metadata" << endl;
-//    }
-//    std::shared_ptr<std::vector<sf::Texture>> cachePtr = resManager->textureCache;
+    ResManager *resManager = new ResManager("res/timemap.json");
+    if(!resManager->init()) {
+        cout << "cannot parse metadata" << endl;
+    }
+    std::shared_ptr<std::vector<sf::Texture>> cachePtr = resManager->textureCache;
     // check the number of arguments
     if(argc == 2) {
         // check the 1st argument
@@ -39,42 +38,42 @@ int main(int argc,char* argv[])
         cout << "you can only say hello" << endl;
     }
 
-//    sf::RenderWindow window(sf::VideoMode(526,601),"test window");
-//
-//
-//
-//    while (window.isOpen()) {
-//
-//        sf::Event event;
-//        while(window.pollEvent(event))
-//        {
-//            if(event.type == sf::Event::Closed)
-//            {
-//                cachePtr.reset();
-//                window.close();
-//                return 0;
-//            }
-//            window.clear();
-//            int x= 0;
-//            int y = 0;
-//            for (int i = 0; i < (int) resManager->getTileCount();i++)
-//            {
-//                auto *sprite = new sf::Sprite();
-//                sprite->setTexture(cachePtr.get()->at(i));
-//                sprite->setPosition(x*resManager->getTileWidth(),y*resManager->getTileHeight());
-//                window.draw(*sprite);
-//                x++;
-//                if (x >(int) resManager->getTileSetColumns()-1)
-//                {
-//                    y++;
-//                    x = 0;
-//                }
-//                delete sprite;
-//
-//            }
-//            window.display();
-//        }
-//    }
+    sf::RenderWindow window(sf::VideoMode(526,601),"test window");
+
+
+
+    while (window.isOpen()) {
+
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                cachePtr.reset();
+                window.close();
+                return 0;
+            }
+            window.clear();
+            int x= 0;
+            int y = 0;
+            for (int i = 0; i < (int) resManager->getTileCount();i++)
+            {
+                auto *sprite = new sf::Sprite();
+                sprite->setTexture(cachePtr.get()->at(i));
+                sprite->setPosition(x*resManager->getTileWidth(),y*resManager->getTileHeight());
+                window.draw(*sprite);
+                x++;
+                if (x >(int) resManager->getTileSetColumns()-1)
+                {
+                    y++;
+                    x = 0;
+                }
+                delete sprite;
+
+            }
+            window.display();
+        }
+    }
 
     return 0;
 }
