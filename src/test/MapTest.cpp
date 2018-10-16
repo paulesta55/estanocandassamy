f83c6da8747bfb4a46821e76f9698a75f687ed07
paulesta55 <paul.estano@ensea.fr>
2018-10-16 11:04:30 +0200

    modify cmakelist

---
+++ src/test/MapTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by paul on 16/10/18.
+//
+
+#include "MapTest.h"
---
+++ src/test/MapTest.h
@@ -0,0 +1,17 @@
+//
+// Created by paul on 16/10/18.
+//
+
+#ifndef ESTANOCANDASSAMY_MAPTEST_H
+#define ESTANOCANDASSAMY_MAPTEST_H
+
+#include "state.h"
+
+class MapTest {
+public:
+
+
+};
+
+
+#endif //ESTANOCANDASSAMY_MAPTEST_H
