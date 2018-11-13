7c077a04cd30332929ed70b896ed9f7e83f40b15
paulesta55 <paul.estano@ensea.fr>
2018-11-13 14:28:57 +0100

    2.final

---
+++ src/shared/ai/HCompare.cpp
@@ -0,0 +1,20 @@
+//
+// Created by paul on 13/11/18.
+//
+
+#include "HCompare.h"
+#include <state.h>
+#include "Node.h"
+using namespace std;
+using namespace ai;
+using namespace state;
+
+
+bool ai::HCompare::operator()(Node n1, Node n2) {
+    if(n1.heuristic < n2.heuristic)
+        return true;
+    if(n1.heuristic == n2.heuristic)
+        return false;
+    return new runtime_error("failed to sort in priority queue");
+}
+
