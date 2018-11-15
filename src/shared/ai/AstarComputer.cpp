3bed07d1ba3c8d776f6410bb88166d28fd0008bc
paulesta55 <paul.estano@ensea.fr>
2018-11-15 22:21:02 +0100

    correct compare operator and modify astarcomputer

--- src/shared/ai/AstarComputer.cpp
+++ src/shared/ai/AstarComputer.cpp
@@ -1,3 +1,5 @@
+#include <utility>
+
 //
 // Created by paul on 13/11/18.
 //
@@ -21,7 +23,10 @@ shared_ptr<Node> AstarComputer::compute() {
 
 
         shared_ptr<Node> n;
-        n = openList.front();
+        n = openList.at(0);
+
+//        shared_ptr<Node> n= nW.lock();
+        closedList.push_back(n);
         openList.erase(openList.begin());
         openList.shrink_to_fit();
         cout << k << " iterations" << endl;
@@ -30,14 +35,23 @@ shared_ptr<Node> AstarComputer::compute() {
         if(n->getPosition().x == objectif->getPosition().x && n->getPosition().y == objectif->getPosition().y)
             return n;
         else {
-            for(const auto &v : n->getAvailableNeigbors(35,map.get())){
+            for( auto v : n->getAvailableNeigbors(35,map.get())){
                 bool passInFor = true;
+                v->cost += n->cost+1;
+                v->heuristic = static_cast<unsigned int>(v->cost + std::sqrt(((int)(objectif->getPosition().x) - (int)(v->getPosition().x)) * ((int)
+                        (objectif->getPosition().x) - (int)(v->getPosition().x)) +((int)(objectif->getPosition().y)-
+                                (int)(v->getPosition().y))*((int)(objectif->getPosition().y)-(int)(v->getPosition().y))));
                 //looking for v in closedList with heuristique < current heuristique
                 for(auto ni:closedList)
                 {
                     // if found don't insert in openList ...
-                    if(ni->getPosition().x == v->getPosition().x && ni->getPosition().y == v->getPosition().y && ni->heuristic < v->heuristic)
+                    if(ni->getPosition().x == v->getPosition().x && ni->getPosition().y == v->getPosition().y)
                     {
+                        if(ni->heuristic>v->heuristic)
+                        {
+                            ni->previousNode = n;
+                            ni->heuristic = v->heuristic;
+                        }
                         passInFor = false;
                     }
                 }
@@ -47,17 +61,19 @@ shared_ptr<Node> AstarComputer::compute() {
                     // same than for closedList
                     for(auto ni: openList)
                     {
-                        if(ni->getPosition().x == v->getPosition().x && ni->getPosition().y == v->getPosition().y && ni->heuristic < v->heuristic)
+                        if(ni->getPosition().x == v->getPosition().x && ni->getPosition().y == v->getPosition().y)
+
                         {
+                            if(ni->heuristic>v->heuristic)
+                            {
+                                ni->previousNode = n;
+                                ni->heuristic = v->heuristic;
+                            }
                             passInFor = false;
                         }
                     }
                 }
                 if(passInFor) {
-                    v->cost += n->cost+1;
-                    v->heuristic = static_cast<unsigned int>(v->cost + std::sqrt(((int)(objectif->getPosition().x) - (int)(v->getPosition().x)) * ((int)
-                                                (objectif->getPosition().x) - (int)(v->getPosition().x)) +((int)(objectif->getPosition().y)-
-                                                        (int)(v->getPosition().y))*((int)(objectif->getPosition().y)-(int)(v->getPosition().y))));
                     openList.push_back(v);
                     std::sort(openList.begin(),openList.end(),HCompare());
                 }
@@ -65,7 +81,7 @@ shared_ptr<Node> AstarComputer::compute() {
             
         }
         k = k+1;
-        closedList.push_back(n);
+
 
     }
 
@@ -75,6 +91,6 @@ shared_ptr<Node> AstarComputer::compute() {
 AstarComputer::AstarComputer(shared_ptr<state::Map> map, Node &objectif, Node &source) {
     this->objectif = make_shared<Node>(objectif);
     this->source = make_shared<Node>(source);
-    this->map = map;
+    this->map = std::move(map);
 }
 
