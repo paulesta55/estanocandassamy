437ed867bef3b2f1eca063a15519119ae22cc337
paul estano <paul.estano@ensea.fr>
2018-12-15 13:27:31 +0100

    implement simple multithreading without mutex

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -12,6 +12,7 @@
 #include <utility>
 #include "engine.h"
 #include "ai.h"
+#include <thread>
 
 void testSFML() {
     sf::Texture texture;
@@ -427,6 +428,82 @@ int main(int argc,char* argv[])
         }
 
     }
+    if(!strcmp(argv[1],"threads")) {
+        cout << "threads" << endl;
+        shared_ptr<Engine> engine = make_shared<Engine>(State(Position(),make_shared<Map>("res/src/etage1.json")));
+
+        // Create some AI players
+        const unsigned int id = 1;
+        unsigned int idPlayer1 = 1;
+        string player = "Alice";
+        const pair<const unsigned int, shared_ptr<Player>> pair1 = make_pair(id,make_shared<Player>(true,player,idPlayer1,make_shared<Bulbizarre>(WEST
+                ,200,Position(3,9))));
+        engine->getState().getPlayers().insert(pair1);
+        const unsigned int id2 = 0;
+        unsigned int idPlayer2 = 0;
+        string player2 = "Bob";
+        const pair<const unsigned int, shared_ptr<Player>> pair2 =make_pair(id2,make_shared<Player>(false,player2,idPlayer2,make_shared<Salameche>(EST
+                ,150,Position(20,20))));
+        engine->getState().getPlayers().insert(pair2);
+        engine->getState().center = Position(7,7);
+
+        // Set up the render
+        shared_ptr<Scene> scene3;
+        scene3.reset(new Scene(engine,"res/src/tilemap2.png",0));
+        engine->getState().registerObserver(scene3.get());
+
+
+        // Call our AI computer
+        shared_ptr<AI> aiTest;
+        aiTest.reset(new DeepAI);
+
+        thread eng([engine,aiTest]{
+            cerr << "engine running" << endl;
+            while(1) {
+                if (!engine->getCommands().empty()) {
+                    for (auto player : engine->getState().getPlayers()) {
+                        if (player.second && player.second->getIA() && player.second->getPokemon()->getAlive()) {
+                            cerr << "run ai" << endl;
+                            aiTest->run(*engine,player.first,0);
+
+                            break;
+                        }
+                    }
+                    engine->runCommands();
+                }
+            }
+
+        });
+       //thread render([engine,scene3] {
+           cerr << "render running" << endl;
+           sf::RenderWindow window(sf::VideoMode(600,600),"test window");
+           while(window.isOpen()) {
+
+               // Look for real living players
+               bool playerAliveFound = false;
+               for (auto player: engine->getState().getPlayers()) {
+                   if (player.second && !(player.second->getIA()) && player.second->getPokemon()->getAlive()) {
+                       playerAliveFound = true;
+                       break;
+                   }
+
+               }
+               // if no real players => game over
+               if (!playerAliveFound) {
+                   cout << "GAME OVER" << endl;
+                   engine->getState().setGameFinished(true);
+                   engine->getState().gameOver = true;
+               }
+               scene3->draw(window);
+
+           }
+   // });
+
+      //  render.join();
+
+
+    }
+
     else {
         cout << "I don't understand"<< endl;
         cout << "you can only say hello" << endl;
