4acd3afa9581a52df87fded401fc0a9aeef38354
paul estano <paul.estano@ensea.fr>
2019-01-05 18:22:57 +0100

    add a server target to cmakelist.txt and makefile

---
+++ src/server/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by paul on 1/5/19.
+//
+#include <iostream>
+#include "engine.h"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "hello server" <<endl;
+    return 0;
+}
